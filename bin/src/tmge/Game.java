72f5a0bfe65bf1c37ac9402a2adf191943ff0abf
Lawrence Hwang <lawrenceh326@gmail.com>
2021-02-25 19:38:33 -0800

    Basic grid and game TMGE

---
+++ bin/src/tmge/Game.java
@@ -0,0 +1,6 @@
+package tmge;
+
+public class Game {
+	
+	
+}
---
+++ bin/src/tmge/Grid.java
@@ -0,0 +1,10 @@
+package tmge;
+
+public class Grid {
+	private int[][] grid;
+	
+	public Grid(int rows, int cols) {
+		grid = new int[rows][cols];
+	}
+	
+}
