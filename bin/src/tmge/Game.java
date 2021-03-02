3782f0f8ab1dc032599a33cdb6412eb771535b75
Lawrence Hwang <lawrenceh326@gmail.com>
2021-03-01 16:07:08 -0800

    TMGE Game to abstract

--- bin/src/tmge/Game.java
+++ bin/src/tmge/Game.java
@@ -1,6 +1,16 @@
 package tmge;
 
-public class Game {
+public abstract class Game {
+	private Grid grid;
+//	private Events events;
+	private int score;
+	
+	public Game() {
+		this.score = 0;
+		// this.grid = new Grid(x, y);
+		// this.events = new Events();
+	}
 	
 	
+	public abstract void update();
 }
