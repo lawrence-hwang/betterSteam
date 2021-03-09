028427e1f9bded282d17edf3b5abbc4a45d7ed1b
David B. Rodriguez <davidbr@uci.edu>
2021-03-08 19:50:39 -0800

    Added Events and Edited Game
    Created Events abstract class and added private variables to Game

--- tmge/src/tmge/Game.java
+++ tmge/src/tmge/Game.java
@@ -2,6 +2,9 @@ package tmge;
 
 public class Game {
     private Grid grid;
+    private Events event;
+    private int Score;
+    // private for GUI if we get to it
 
     public Game() {
         this.grid = new Grid(5, 5);
