abcbc5b46ec0ac6345e34dc90426046ece4fc992
Lawrence Hwang <lawrenceh326@gmail.com>
2021-03-08 19:38:51 -0800

    Create GameManager in tmge

---
+++ tmge/src/tmge/GameManager.java
@@ -0,0 +1,14 @@
+package tmge;
+
+public class GameManager {
+    private Game[] games;
+    private int players;
+    private String gameInstance;
+
+    public GameManager() {
+    }
+
+    public void setPlayers(int players) {
+        this.players = players;
+    }
+}
