75bcfcfaae8daa063b1c100699959371022aced3
Lawrence Hwang <lawrenceh326@gmail.com>
2021-03-08 19:06:49 -0800

    Creating PR for tmge

--- tmge/src/tmge/Main.java
+++ tmge/src/tmge/Main.java
@@ -2,6 +2,7 @@ package tmge;
 
 public class Main {
     public static void main(String args[]) {
+        // create pull req
         Game test = new Game();
         System.out.println(test.getGrid().printGrid());
     }
